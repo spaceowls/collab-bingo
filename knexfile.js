7a6a7cbda41b275ef4423904073835667a937d46
llofyy <lucasandrefreitas1@gmail.com>
2021-09-10 17:56:48 -0300

    Banco de dados portado para o Postgres

--- knexfile.js
+++ knexfile.js
@@ -4,13 +4,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {
-      host: "ec2-52-7-159-155.compute-1.amazonaws.com",
-      port: "5432",
-      user: 'cncylttkibfjyx',
-      password: '18b50217c059769ba98673874c8e0b928aa7aa13b4351c8f10e0a39bb85fc277',
-      database: 'ddos6s7cdhss6c'    
-    },
+    connection: process.env.DATABASE_URL,
     useNullAsDefault: true,
     migrations: {
       directory: `${__dirname}/src/database/migrations`
