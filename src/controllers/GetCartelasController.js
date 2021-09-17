f34c5e51d602b454b34706ba59dc5ed12ea4460e
llofyy <lucasandrefreitas1@gmail.com>
2021-09-17 01:17:30 -0300

    alguns bugs resolvidos

--- src/controllers/GetCartelasController.js
+++ src/controllers/GetCartelasController.js
@@ -5,31 +5,33 @@ const GetUserService = require('../services/users/GetUserService');
 async function GetCartelas(req, res) {
     const { user_id } = req.params;
     const { userAuthenticated } = req.body;
-    const response = await GetCartelaService(user_id).catch((error) => {
+    await GetCartelaService(user_id)
+    .then(async (response) => {
+        let user;
+        if(userAuthenticated.username){
+            user = userAuthenticated
+        }else{
+            user = await GetUserService(userAuthenticated.user_id);
+        }
+
+        const sala = await GetRoomService(response.cartela.bingo_id)
+
+      res.render('telaBingo', {
+        user: {
+            id: userAuthenticated.username ? userAuthenticated.user_id : user.id,
+            ...user
+        },
+          cartela: response.cartela ? response.cartela : response.status,
+          criador: sala.room.user_id,
+          sala: sala.room.id,
+          sala_name: sala.room.name
+      });
+    })
+    .catch((error) => {
       if(error) {
         res.redirect('/')
       }
     })
-    
-    let user;
-    if(userAuthenticated.username){
-        user = userAuthenticated
-    }else{
-        user = await GetUserService(userAuthenticated.user_id);
-    }
-
-    const sala = await GetRoomService(response.cartela.bingo_id)
-
-  res.render('telaBingo', {
-    user: {
-        id: userAuthenticated.username ? userAuthenticated.user_id : user.id,
-        ...user
-    },
-      cartela: response.cartela ? response.cartela : response.status,
-      criador: sala.room.user_id,
-      sala: sala.room.id,
-      sala_name: sala.room.name
-  });
 }
 
 module.exports = GetCartelas;
