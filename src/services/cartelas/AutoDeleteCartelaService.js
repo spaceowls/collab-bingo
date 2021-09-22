041e8dcdf965fc59da0f0ea2591c313a9bcb41b7
llofyy <lucasandrefreitas1@gmail.com>
2021-09-22 19:13:06 -0300

    Configuração do banco de dados

--- src/services/cartelas/AutoDeleteCartelaService.js
+++ src/services/cartelas/AutoDeleteCartelaService.js
@@ -1,7 +1,7 @@
 const knex = require('../../database');
 
 async function AutoDeleteCartelaService(owner_user) {
-    const existCartela = await knex('cartela').where({ owner_user }).select().then(cartela => cartela[0]);
+    const existCartela = await knex('cartelas').where({ owner_user }).select().then(cartela => cartela[0]);
 
     if(existCartela) {
         await knex('cartelas').where({owner_user: existCartela.owner_user}).delete();
